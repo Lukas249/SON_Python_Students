9d191ae6fd40d6ee17adb7b4f9e797b2c5e87166
Łukasz Radecki <lukaszradecki6@gmail.com>
2024-11-29 16:18:36 +0100

    feat: add export method and implement automatic save after changes

--- students/students_manager.py
+++ students/students_manager.py
@@ -1,17 +1,27 @@
+from students.export_students import BaseExportStudents
 from students.student import Student
 
 class StudentsManager:
 
-    def __init__(self, students: list[Student]):
+    def __init__(self, students: list[Student], exporters: list[BaseExportStudents]):
         self.students = students
+        self.exporters = exporters
 
-    def add_student(self, student: Student) -> bool:
+    def __add(self, student: Student) -> bool:
         if self.student_exists(student.id):
             return False
 
         self.students.append(student)
 
         return True
+
+    def add_student(self, student: Student) -> bool:
+        student_added = self.__add(student)
+
+        if student_added:
+            self.export()
+
+        return student_added
 
     def student_exists(self, student_id: str) -> bool:
         for student in self.students:
@@ -20,7 +30,7 @@ class StudentsManager:
 
         return False
 
-    def modify_student(self, student_id: str, student_name: str, student_surname: str) -> bool:
+    def __modify(self, student_id: str, student_name: str, student_surname: str) -> bool:
         if not self.student_exists(student_id):
             return False
 
@@ -31,8 +41,16 @@ class StudentsManager:
 
         return True
 
+    def modify_student(self, student_id: str, student_name: str, student_surname: str) -> bool:
+        student_modified = self.__modify(student_id, student_name, student_surname)
 
-    def delete_student(self, student_id: str) -> bool:
+        if student_modified:
+            self.export()
+
+        return student_modified
+
+
+    def __delete(self, student_id):
         for i in range(len(self.students)):
             if self.students[i].id == student_id:
                 del self.students[i]
@@ -40,7 +58,17 @@ class StudentsManager:
 
         return False
 
+    def delete_student(self, student_id: str) -> bool:
+        student_deleted = self.__delete(student_id)
 
+        if student_deleted:
+            self.export()
+
+        return student_deleted
+
+    def export(self):
+        for exporter in self.exporters:
+            exporter.export(self.students)
 
 
 
