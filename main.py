047aabd790ce0bc82b549e20ccda97186295d3d4
Łukasz Radecki <lukaszradecki6@gmail.com>
2024-11-29 16:13:53 +0100

    refactor: adjust code to reflect recent changes

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import datetime
 
 from attendance.attendance import Attendance
-from attendance.format_attendance import FormatAttendance
-from attendance.parse_attendance import ParseAttendance
-from students.format_students import FormatStudents
-from students.parse_students import ParseStudents
+from attendance.format_attendance import FormatAttendanceTXT
+from attendance.parse_attendance import ParseAttendanceTXT
+from students.export_students import ExportStudents
+from students.format_students import FormatStudentsTXT, FormatStudentsCSV
+from students.parse_students import ParseStudentsTXT, ParseStudentsCSV
 from file_handler import FileHandler
 from students.student import Student
 from students.students_manager import StudentsManager
@@ -14,14 +15,12 @@ pathCSV = "lists/student_list.csv"
 pathTXT = "lists/student_list.txt"
 
 # import
-# students_list = ParseStudents.csv_to_list(FileHandler.load(pathCSV))
-students_list = ParseStudents.txt_to_list(FileHandler.load(pathTXT))
+# students_list = ParseStudentsCSV.parse(FileHandler.load(pathCSV))
+students_list = ParseStudentsTXT.parse(FileHandler.load(pathTXT))
 
-'''
-for students in lista:
-    print(students.get("Name"), students.get("Surname"), students.get("ID"))
 
-'''
+# for students in students_list:
+#     print(students.name, students.surname, students.id)
 
 '''
 for students in lista2:
@@ -29,9 +28,21 @@ for students in lista2:
 '''
 
 # # students manager
-manage_students = StudentsManager(students_list)
-
+# students_txt_exporter = ExportStudents(
+#     file_saver=FileHandler(),
+#     formatter=FormatStudentsTXT(),
+#     export_path=pathTXT
+# )
 #
+# students_csv_exporter = ExportStudents(
+#     file_saver=FileHandler(),
+#     formatter=FormatStudentsCSV(),
+#     export_path=pathCSV
+# )
+#
+# manage_students = StudentsManager(students_list, [students_txt_exporter, students_csv_exporter])
+
+
 # # add students
 # new_student = Student(
 #     input("Enter students's name: "),
@@ -53,11 +64,7 @@ manage_students = StudentsManager(students_list)
 # student_id_to_delete = input("Enter students's ID to delete: ")
 # manage_students.delete_student(student_id_to_delete)
 
-# print all students
-# for students in students_list:
-#     print(students.name, students.surname, students.id)
-
-# # attendance
+# attendance
 # date = datetime.datetime(2024, 11, 24)
 # attendance_path = "lists/attendance.txt"
 #
@@ -65,23 +72,15 @@ manage_students = StudentsManager(students_list)
 # attendance.modify_attendance(students_list[0].id, True)
 #
 # # format attendance
-# txt_format_attendance = FormatAttendance.format_to_txt(attendance)
+# txt_format_attendance = FormatAttendanceTXT.format(attendance)
 #
 # # export attendance
 # FileHandler.save(attendance_path, txt_format_attendance)
-
+#
 # # # import and parse attendance
 # attendance_path = "lists/attendance.txt"
-# attendance = ParseAttendance.txt(FileHandler.load(attendance_path))
+# attendance = ParseAttendanceTXT.parse(FileHandler.load(attendance_path))
 #
 # print(attendance.date)
 # print(attendance.presence)
 # print([x.dict(["Name", "Surname", "ID"]) for x in attendance.students])
-
-# # format students
-# csv_format_student_list = FormatStudents.format_to_csv(students_list)
-# txt_format_student_list = FormatStudents.format_to_txt(students_list)
-#
-# # export students
-# FileHandler.save(pathCSV, csv_format_student_list)
-# FileHandler.save(pathTXT, txt_format_student_list)
