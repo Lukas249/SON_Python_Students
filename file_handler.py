5f73b7bf3bd459127adab8da99c829fad30df59a
Łukasz Radecki <lukaszradecki6@gmail.com>
2024-11-29 16:11:59 +0100

    feat: create abstract classes for saving and loading, and make FileHandler depend on them

--- file_handler.py
+++ file_handler.py
@@ -1,10 +1,27 @@
-class FileHandler:
+from abc import abstractmethod
+
+class BaseFileSaver:
+
+    @staticmethod
+    @abstractmethod
+    def save(path: str, lines: list[str]):
+        pass
+
+
+class BaseFileLoader:
+
+    @staticmethod
+    @abstractmethod
+    def load(path: str):
+        pass
+
+class FileHandler(BaseFileSaver, BaseFileLoader):
+
     @staticmethod
     def load(path: str):
         file = open(path, "r")
         lines = file.readlines()
         file.close()
-
         return lines
 
     @staticmethod
